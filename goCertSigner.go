fd47f9bf85511b33fc398269da395ba503bc9f2b
Noah <nms277@gmail.com>
2014-07-08 16:03:10 +0900

    updated repo and package names

--- goCertSigner.go
+++ goCertSigner.go
@@ -1,4 +1,4 @@
-package opensslSign
+package goCertSigner
 
 /*
 #cgo !windows LDFLAGS: -lcrypto
@@ -33,12 +33,7 @@ import "C"
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"unsafe"
@@ -262,88 +257,3 @@ func SignDocument(doc []byte, pkcs12 []byte, pkcsPass string, caCert []byte) (si
 	return signDoc(&kc, doc), err //sign the document
 
 }
-
-//////////////////////////////////////////////////////////////////////////
-//
-// encrypt string to base64 crypto using AES
-//
-//
-//////////////////////////////////////////////////////////////////////////
-func encryptAESCFB(key []byte, text string) string {
-
-	plaintext := []byte(text)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-
-	// convert to base64
-	return base64.URLEncoding.EncodeToString(ciphertext)
-}
-
-//////////////////////////////////////////////////////////////////////////
-//
-// decrypt from base64 to decrypted string
-//
-//
-//////////////////////////////////////////////////////////////////////////
-func decryptAESCFB(key []byte, cryptoText string) string {
-	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if len(ciphertext) < aes.BlockSize {
-		panic("ciphertext too short")
-	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	// XORKeyStream can work in-place if the two arguments are the same.
-	stream.XORKeyStream(ciphertext, ciphertext)
-
-	return fmt.Sprintf("%s", ciphertext)
-}
-
-//////////////////////////////////////////////////////////////////////////
-//
-// Authentication
-// createMac creates a message authentication code (MAC).
-//
-//////////////////////////////////////////////////////////////////////////
-//func createMac(h hash.Hash, value []byte) []byte {
-//	h.Write(value)
-//	return h.Sum(nil)
-//}
-
-//////////////////////////////////////////////////////////////////////////
-//
-// Authentication
-// verifyMac verifies that a message authentication code (MAC) is valid.
-//
-//////////////////////////////////////////////////////////////////////////
-//func verifyMac(h hash.Hash, value []byte, mac []byte) error {
-//	mac2 := createMac(h, value)
-//	if len(mac) == len(mac2) && subtle.ConstantTimeCompare(mac, mac2) == 1 {
-//		return nil
-//	}
-//	return errors.New("verifyMac: the value is not valid")
-//}
