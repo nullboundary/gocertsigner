4c279d3ea2c91ba666c4f66513363127e9ec8445
Noah Shibley <archive.of.receipts@gmail.com>
2015-07-21 15:52:29 +0900

    Refactored error handling
    Now more idiomatic go

--- gocertsigner.go
+++ gocertsigner.go
@@ -34,16 +34,13 @@ static void X509PopFree(STACK_OF(X509) *ca) {
 import "C"
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"unsafe"
 )
 
 //KeysAndCerts is a struct to store all openssl types and passwords used for signing.
 type KeysAndCerts struct {
-	Password string      //password for the p12 cert
+	password []byte      //password for the p12 cert
 	scert    *C.X509     //cert from the p12 cert
 	skey     *C.EVP_PKEY //key from the p12 cert
 	ca       *C.STACK    //stack of certificate authorities. CA from 2 sources
@@ -57,42 +54,23 @@ func init() {
 }
 
 //free cleans up all openssl certificate and key allocated memory
-func free(kc *KeysAndCerts) {
+func (kc *KeysAndCerts) free() {
+	kc.clearPassword()
 	C.X509PopFree(kc.ca)
 	C.X509_free(kc.scert)
 	C.EVP_PKEY_free(kc.skey)
 }
 
-//FileToBytes is a convience function for loading a file and returning a slice of bytes.
-func FileToBytes(fileName string) []byte {
-
-	//open file
-	file, err := os.Open(fileName)
-	if err != nil {
-		panic(err)
+//clearPassword writes over the memory where the password was stored.
+func (kc *KeysAndCerts) clearPassword() {
+	for i := 0; i < len(kc.password); i++ {
+		kc.password[i] = 0
 	}
-	defer file.Close()
-
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(file)
-
-	return buf.Bytes()
-
-}
-
-//BytesToFile is a convience function for saving a slice of bytes to a file.
-func BytesToFile(fileName string, outBytes []byte) {
-
-	err := ioutil.WriteFile(fileName, outBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
-
 }
 
 //parse12 reads the Pkcs12 certificate byte slice and parses it into a
 //openssl *C.X509 certificate type and an openssl *C.EVP_PKEY type.
-func parseP12(kc *KeysAndCerts, p12Bytes []byte) {
+func (kc *KeysAndCerts) parseP12(p12Bytes []byte) error {
 
 	var p12 *C.PKCS12
 	kc.scert = nil
@@ -104,10 +82,10 @@ func parseP12(kc *KeysAndCerts, p12Bytes []byte) {
 	p12Buf := ((*C.uchar)(unsafe.Pointer(&p12Bytes[0]))) //go []bytes to C * unsigned char
 
 	if C.d2i_PKCS12(&p12, &p12Buf, p12BufLen) == nil {
-		panic("p12 convert error")
+		return fmt.Errorf("p12 convert error")
 	}
 
-	pass := C.CString(kc.Password)
+	pass := C.CString(string(kc.password))
 	defer C.free(unsafe.Pointer(pass))
 
 	if C.PKCS12_parse(p12, pass, &kc.skey, &kc.scert, nil) == 0 { //parse pkcs12 into 3 files
@@ -115,22 +93,24 @@ func parseP12(kc *KeysAndCerts, p12Bytes []byte) {
 		var cstr [256]C.char
 		C.ERR_error_string(C.ERR_peek_error(), &cstr[0])
 		errorStr := C.GoString(&cstr[0])
-		panic(errorStr)
+		return fmt.Errorf("%s", errorStr)
 	}
 
 	if kc.skey == nil {
-		panic("skey == nil")
+		return fmt.Errorf("p12 key is nil")
 	}
 
 	if kc.scert == nil {
-		panic("scert == nil")
+		return fmt.Errorf("p12 cert is nil")
 	}
+
+	return nil
 
 }
 
 //parseX509Cert reads the X509 der formatted certificate byte slice and parses it into a
 //openssl *C.X509 type.
-func parseX509Cert(kc *KeysAndCerts, certX509 []byte) {
+func (kc *KeysAndCerts) parseX509Cert(certX509 []byte) error {
 
 	kc.scert = nil
 	certBufLen := C.long(len(certX509))
@@ -142,17 +122,19 @@ func parseX509Cert(kc *KeysAndCerts, certX509 []byte) {
 		C.ERR_error_string(C.ERR_peek_error(), &cstr[0]) //get openssl error code
 		errorStr := C.GoString(&cstr[0])
 		errorStr = "parse Cert Fail:" + errorStr
-		panic(errorStr)
+		return fmt.Errorf("%s", errorStr)
 	}
 	if kc.scert == nil {
-		panic("kc.scert == nil")
+		return fmt.Errorf("X509 cert is nil")
 	}
+
+	return nil
 
 }
 
 //parsePrivateKeyPem reads the private key pem byte slice and parses it into a
 //openssl *C.EVP_PKEY type.
-func parsePrivateKeyPem(kc *KeysAndCerts, privKeyPem []byte) {
+func (kc *KeysAndCerts) parsePrivateKeyPem(privKeyPem []byte) error {
 
 	kc.skey = nil
 
@@ -163,24 +145,26 @@ func parsePrivateKeyPem(kc *KeysAndCerts, privKeyPem []byte) {
 	privKeyBio := C.BIO_new_mem_buf(pemBuf, pemBufLen)
 	defer C.BIO_free(privKeyBio)
 	if privKeyBio == nil {
-		panic("new openssl pem bio error")
+		return fmt.Errorf("error making key pem buffer")
 	}
 
-	pass := C.CString(kc.Password)
+	pass := C.CString(string(kc.password))
 	defer C.free(unsafe.Pointer(pass))
 
 	if C.PEM_read_bio_PrivateKey(privKeyBio, &kc.skey, nil, unsafe.Pointer(pass)) == nil {
 		var cstr [256]C.char
 		C.ERR_error_string(C.ERR_peek_error(), &cstr[0]) //get openssl error code
 		errorStr := C.GoString(&cstr[0])
-		panic(errorStr)
+		return fmt.Errorf("%s", errorStr)
 	}
+
+	return nil
 
 }
 
 //addCA reads the intermediate & CA root certificates and
 //adds them to a X509 cert stack.
-func addCA(kc *KeysAndCerts, ca []byte) {
+func (kc *KeysAndCerts) addCA(ca []byte) error {
 	/* Read intermediate & CA root certificates */
 
 	var caCert *C.X509
@@ -191,11 +175,11 @@ func addCA(kc *KeysAndCerts, ca []byte) {
 	caBuf := ((*C.uchar)(unsafe.Pointer(&ca[0]))) //bytes[] to * unsigned char
 
 	if C.d2i_X509(&caCert, &caBuf, caBufLen) == nil {
-		panic("add CA fail")
+		return fmt.Errorf("error adding CA to X509 cert stack")
 	}
 
 	if caCert == nil {
-		panic("caCert == nil")
+		return fmt.Errorf("CA Cert is nil")
 	}
 
 	//Add the intermediate CA certificate to the signing stack
@@ -204,10 +188,12 @@ func addCA(kc *KeysAndCerts, ca []byte) {
 	}
 	C.X509Push(kc.ca, caCert)
 
+	return nil
+
 }
 
 //signDoc uses the KeysAndCerts struct to create a pkcs7 signature.
-func signDoc(kc *KeysAndCerts, document []byte) []byte {
+func (kc *KeysAndCerts) signDoc(document []byte) ([]byte, error) {
 
 	flags := C.int(C.PKCS7_DETACHED | C.PKCS7_BINARY)
 
@@ -218,14 +204,14 @@ func signDoc(kc *KeysAndCerts, document []byte) []byte {
 	doc := C.BIO_new_mem_buf(documentBuf, documentBufLen)
 	defer C.BIO_free(doc)
 	if doc == nil {
-		panic("new openssl bio error")
+		return nil, fmt.Errorf("error making document buffer")
 	}
 
 	//sign the data and create a pkcs7
 	p7 := C.PKCS7_sign(kc.scert, kc.skey, kc.ca, doc, flags)
 	defer C.PKCS7_free(p7)
 	if p7 == nil {
-		panic("p7 sign error")
+		return nil, fmt.Errorf("p7 signing error")
 	}
 
 	//output pkcs7 to []byte
@@ -234,60 +220,65 @@ func signDoc(kc *KeysAndCerts, document []byte) []byte {
 
 	len := C.i2d_PKCS7(p7, &buf)
 	if len == 0 {
-		panic("i2d Pkcs7 error")
+		return nil, fmt.Errorf("error converting pkcs7 to byte buffer")
 	}
 
-	return C.GoBytes(unsafe.Pointer(buf), len)
+	return C.GoBytes(unsafe.Pointer(buf), len), nil
 
 }
 
 //SignWithP12 signs a document using a p12 cert producing a pkcs7 signature.
-func SignWithP12(doc []byte, pkcs12 []byte, pkcsPass string, caCert []byte) (signature []byte, err error) {
-
-	//recover from panics and return error messages
-	defer func() {
-		if r := recover(); r != nil {
-			var ok bool
-			err, ok = r.(error)
-			if !ok {
-				err = fmt.Errorf("signer: %v", r)
-			}
-		}
-	}()
+func SignWithP12(doc []byte, pkcs12 []byte, pkcsPass string, caCert []byte) ([]byte, error) {
 
 	var kc KeysAndCerts
-	defer free(&kc) //free the C allocated memory
+	defer kc.free() //free the C allocated memory
 
-	kc.Password = pkcsPass //set the password to open the encrypted pkcs12 cert
+	kc.password = []byte(pkcsPass) //set the password to open the encrypted pkcs12 cert
 
-	addCA(&kc, caCert)            //add the intermediate CA certificate
-	parseP12(&kc, pkcs12)         //parse the p12 into various keys and certificates
-	return signDoc(&kc, doc), err //sign the document
+	//add the intermediate CA certificate
+	err := kc.addCA(caCert)
+	if err != nil {
+		return nil, err
+	}
+
+	//parse the p12 into various keys and certificates
+	err = kc.parseP12(pkcs12)
+	if err != nil {
+		return nil, err
+	}
+
+	//sign the document
+	return kc.signDoc(doc)
 
 }
 
 //SignWithX509PEM signs a document using a x509 der formatted certificate with a pem formatted key producing a pkcs7 signature.
-func SignWithX509PEM(doc []byte, x509Cert []byte, pemKey []byte, keyPass string, caCert []byte) (signature []byte, err error) {
-
-	//recover from panics and return error messages
-	defer func() {
-		if r := recover(); r != nil {
-			var ok bool
-			err, ok = r.(error)
-			if !ok {
-				err = fmt.Errorf("signer: %v", r)
-			}
-		}
-	}()
+func SignWithX509PEM(doc []byte, x509Cert []byte, pemKey []byte, keyPass string, caCert []byte) ([]byte, error) {
 
 	var kc KeysAndCerts
-	defer free(&kc) //free the C allocated memory
+	defer kc.free() //free the C allocated memory
 
-	kc.Password = keyPass //set the password to open the encrypted pem private key
+	kc.password = []byte(keyPass) //set the password to open the encrypted pem private key
 
-	addCA(&kc, caCert)              //add the intermediate CA certificate
-	parseX509Cert(&kc, x509Cert)    //parse the x509 Der to get the certificate
-	parsePrivateKeyPem(&kc, pemKey) //parse the pem private key
-	return signDoc(&kc, doc), err   //sign the document
+	//add the intermediate CA certificate
+	err := kc.addCA(caCert)
+	if err != nil {
+		return nil, err
+	}
+
+	//parse the x509 Der to get the certificate
+	err = kc.parseX509Cert(x509Cert)
+	if err != nil {
+		return nil, err
+	}
+
+	//parse the pem private key
+	err = kc.parsePrivateKeyPem(pemKey)
+	if err != nil {
+		return nil, err
+	}
+
+	//sign the document
+	return kc.signDoc(doc)
 
 }
